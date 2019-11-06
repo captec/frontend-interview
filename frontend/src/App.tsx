235d18431ded84a2f80680bf770da5a12be23570
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-06 19:53:56 +0100

    feat: improved default branding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/captec/frontend-interview"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Sgit adtarted
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
