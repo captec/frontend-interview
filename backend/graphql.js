3525bc55f387fdd7ef80959272e831cd8d5d66d0
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-07 00:51:20 +0100

    feat: added initial database based graphql graph

--- backend/graphql.js
+++ backend/graphql.js
@@ -1,14 +1,45 @@
 const { ApolloServer, gql } = require('apollo-server-express')
+const { companies, users } = require('./database.json')
 
 const typeDefs = gql`
+  type User {
+    _id: String!
+    name: String!
+    email: String!
+    phone: String!
+    company: Company!
+  }
+
+  type Company {
+    _id: String!
+    name: String!
+    website: String!
+    numberOfEmployees: Int!
+    contact: User!
+  }
+
   type Query {
-    hello: String
+    companies: [Company!]!
+    company (id: String!): Company
+    users: [User!]!
+    user (id: String!): User
   }
 `
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    companies: () => companies,
+    company: (root, { id }) => companies.find(({ _id }) => _id === id),
+    users: () => users,
+    user: (root, { id }) => users.find(({ _id }) => _id === id)
+  },
+
+  Company: {
+    contact: ({ contact }) => users.find(({ _id }) => _id === contact),
+  },
+
+  User: {
+    company: user => companies.find(({ _id }) => _id === user._id)
   }
 }
 
