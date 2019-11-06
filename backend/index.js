e23c942809217fae5555430a047db7be6c72b0b7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-11-06 20:17:17 +0100

    feat: added sample database and /companies endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const database = require('./database.json')
 
 const port = process.env.PORT || 3030
 
 express()
   .get('/status', (req, res) => res.send('ok'))
+  .get('/companies', (req, res) => res.json(database.companies))
   .listen(port, () => console.log(`Backend listening on port ${port}!`))
